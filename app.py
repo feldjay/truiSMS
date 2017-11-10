4cdb96ed7e4f3f34cf9611b29fbba24fb438731c
Jenna Feldman <jennafeldman@gmail.com>
2017-11-10 12:42:48 -0500

    Load csv outside sms handler.

--- app.py
+++ app.py
@@ -6,12 +6,13 @@ from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
+with open('truisms.csv', 'r') as f:
+    reader = csv.reader(f)
+    truisms = list(reader)
+
 
 @app.route('/sms', methods=['POST'])
 def sms():
-    with open('truisms.csv', 'r') as f:
-        reader = csv.reader(f)
-        truisms = list(reader)
 
     truism = random.choice(truisms)[0]
 
