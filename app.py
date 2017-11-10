a6a5445d186d0ea06818c8cd460afb3c0af4ee3a
Jenna Feldman <jennafeldman@gmail.com>
2017-11-09 19:37:42 -0500

    Don't use client

--- app.py
+++ app.py
@@ -2,13 +2,9 @@ import csv
 import random
 from flask import Flask
 from twilio.twiml.messaging_response import MessagingResponse
-from twilio.rest import Client
 
 
 app = Flask(__name__)
-app.config.from_envvar('APP_CONFIG')
-
-client = Client(app.config['TWILIO_ACCOUNT_SID'], app.config['TWILIO_AUTH_TOKEN'])
 
 
 @app.route('/sms', methods=['POST'])
